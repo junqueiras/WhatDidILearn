cd3104438858a5a1924a42f8c985a08a3225b3b2
Mariana Junqueira <69199400+junqueiras@users.noreply.github.com>
2020-10-31 14:39:55 -0300

    creating viewHolder

--- app/src/main/java/com/example/whatdidilearn/view/LearnedItemsAdapter.kt
+++ app/src/main/java/com/example/whatdidilearn/view/LearnedItemsAdapter.kt
@@ -2,14 +2,24 @@ package com.example.whatdidilearn.view
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.whatdidilearn.R
 import com.example.whatdidilearn.entities.ItemLearned
 
 class LearnedItemsAdapter: RecyclerView.Adapter<LearnedItemsAdapter.LearnedItemViewHolder>() {
     var data = listOf<ItemLearned>()
 
     inner class LearnedItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val titleItem: TextView = itemView.findViewById(R.id.itemTitle)
+        val descriptionItem: TextView = itemView.findViewById(R.id.itemDescription)
+        val levelItem: View = itemView.findViewById(R.id.itemLevel)
 
+        fun bind(title: String, description: String, color: Int) {
+            titleItem.text = title
+            descriptionItem.text = description
+            levelItem.setBackgroundResource(color)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LearnedItemViewHolder {
