21103a7804d0c16bb6e1ff6190eca3e795ea115b
Mariana Junqueira <69199400+junqueiras@users.noreply.github.com>
2020-11-03 19:51:59 -0300

    adding viewBinding

--- app/src/main/java/com/example/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/com/example/whatdidilearn/view/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.whatdidilearn.view
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import com.example.whatdidilearn.R
 import com.example.whatdidilearn.data.DatabaseItems
+import com.example.whatdidilearn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.items_recycle_view)
+        val recyclerView = binding.itemsRecycleView
         val adapter = LearnedItemsAdapter()
         recyclerView.adapter = adapter
 
